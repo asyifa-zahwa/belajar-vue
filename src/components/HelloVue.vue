274a861f76f8987273bc50d48e445f022ea6722e
Asyifa-Zahwa <79799179+asyifa-zahwa@users.noreply.github.com>
2025-06-05 14:24:53 +0700

    first commit

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<script setup>
+    console.info('Hello Vue!');
+</script>
+<template>
+    <h1 class="hello">hello vue</h1>
+</template>
+<style scoped>
+    .hello {
+        color: #42b983;
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
---
+++ src/hello.js
@@ -0,0 +1,3 @@
+import { createApp } from "vue";
+import Hellovue from "./components/HelloVue.vue";
+createApp(Hellovue).mount("#app");
